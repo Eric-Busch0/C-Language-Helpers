2f342148074364eea012d09d314712b75ba71872
EricBusch-CDI <74311902+EricBusch-CDI@users.noreply.github.com>
2021-02-25 13:58:14 -0600

    update

--- Generics/generic_macros.h
+++ Generics/generic_macros.h
@@ -7,5 +7,20 @@
 #define zeroArr(arr, size) memset(arr, 0, size)
 #define asPercentage(num, den) (int)round((100 * (((double)num) / den)))
 #define arrLen(arr)  (sizeof(arr)/(sizeof(arr[0])))
+
+#ifndef MIN
+#define MIN(x, y) (x < y ? x : y)
+#endif
+
+#ifndef MAX
+#define MAX(x, y) (x > y ? x : y)
+#endif
+
+// return 1 if little endian
+#define isLittleEndian() (((*(short *)"21") & 0xFF) == '2')
+
+// return 1 if big endian
+#define IS_BIG_ENDIAN() (((*(short *)"21") & 0xFF) == '1')
+
 #define put_endl printf("\n")
 #endif
