850b284149793d7a761bb1b8d49a3a0456ca642c
EricBusch-CDI <74311902+EricBusch-CDI@users.noreply.github.com>
2021-02-25 16:27:11 -0600

    update

--- Generics/generic_functions.c
+++ Generics/generic_functions.c
@@ -1,8 +1,13 @@
 #include "generic_functions.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
 float *create_uniform_array(size_t size)
 {
+    /* Creates and fills an array allocated on the heap */
+
     float *arr = malloc(sizeof(float) * size);
 
     for (size_t i = 0; i < size; i++)
@@ -11,11 +16,55 @@ float *create_uniform_array(size_t size)
     }
     return arr;
 }
+
+void fill_array_uniform(float *arr, size_t size)
+{
+    /* Fills given array from 0 to size - 1 */
+
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = i;
+    }
+}
+
 void print_array(float *arr, size_t size)
 {
+    /*Prints the values of an array */
+    
+    printf("\n\n");
+
     for (size_t i = 0; i < size; i++)
     {
         printf("%f\n", arr[i]);
     }
+
     printf("\n\n");
+}
+
+bool is_in_string(char *str, char *substring)
+{
+    /* Determines whether the substring is inside the string (case insensistive) */
+
+    if (strcasestr(str, substring) != NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool is_in_string_case(char *str, char *substring)
+{
+    /* Determines whether the substring is inside the string (case sensistive) */
+
+    if (strstr(str, substring) != NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
--- Generics/generic_functions.h
+++ Generics/generic_functions.h
@@ -1,7 +1,11 @@
 #ifndef GENERIC_FUNCTIONS_H
 #define GENERIC_FUNCTIONS_H
 #include <stdlib.h>
+#include <stdbool.h>
 
 float *create_uniform_array(size_t size);
 void print_array(float *arr, size_t size);
+bool is_in_string(char* str, char* substring);
+bool is_in_string_case(char* str, char* substring);
+
 #endif
